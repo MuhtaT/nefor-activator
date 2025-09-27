e84ccb966999f01d90f74fe8c6f003df1409c006
Muhtat <tifixixr@gmail.com>
2025-09-27 20:08:04 +0300

    API maven repo upload

--- activator-api/build.gradle.kts
+++ activator-api/build.gradle.kts
@@ -1,9 +1,31 @@
 plugins {
     id("java-library")
+    `maven-publish`
 }
 
 java {
     withSourcesJar()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("gpr") {
+            from(components["java"])
+            groupId = "dev.nefor.activator"
+            artifactId = "activator-api"
+            version = project.version.toString()
+        }
+    }
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/Muhtat/NeforActivator")
+            credentials {
+                username = findProperty("gpr.user")?.toString() ?: System.getenv("GITHUB_USERNAME")
+                password = findProperty("gpr.key")?.toString() ?: System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
 }
 
 dependencies {
